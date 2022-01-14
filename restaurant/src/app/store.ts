4876090b2996342be28bd0c8751678d0cac13ea3
hyoni0817 <hyoni0817@naver.com>
2022-01-14 22:50:20 +0900

    Feat: store 및 reservationSlice 생성

--- restaurant/src/app/store.ts
+++ restaurant/src/app/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import reservationsReducer from "../features/reservationSlice";
 
 export const store = configureStore({
   reducer: {
     // 리듀서 내부에 모든 다른 슬라이스를 가짐.
+    reservations: reservationsReducer,
   },
 });
 
---
+++ restaurant/src/features/reservationSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface ReservationState {
+  value: string[];
+}
+const initialState: ReservationState = {
+  value: ["Selena"],
+};
+
+export const reservationsSlice = createSlice({
+  name: "reservations",
+  initialState,
+  reducers: {
+    addReservation: (state, action: PayloadAction<string>) => {
+      state.value.push(action.payload);
+    },
+  },
+});
+
+export const { addReservation } = reservationsSlice.actions;
+
+export default reservationsSlice.reducer;
