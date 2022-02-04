eacf5eff7bab0297675bf8ed11a897cbfa077d83
hyoni0817 <hyoni0817@naver.com>
2022-02-04 20:25:09 +0900

    Feat: CustomerSlice 생성

--- restaurant/src/app/store.ts
+++ restaurant/src/app/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reservationsReducer from "../features/reservationSlice";
+import customerReducer from "../features/customerSlice";
 
 export const store = configureStore({
   reducer: {
     // 리듀서 내부에 모든 다른 슬라이스를 가짐.
     reservations: reservationsReducer,
+    customers: customerReducer,
   },
 });
 
---
+++ restaurant/src/features/customerSlice.ts
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface CustomerState {
+  value: string[];
+}
+
+interface Customer {
+  id: string;
+  name: string;
+  food: string[];
+}
+
+const initialState: CustomerState = {
+  value: [],
+};
+
+
+export const customerSlice = createSlice({
+  name: "customers",
+  initialState,
+  reducers: {
+
+  },
+});
+
+export const { } = customerSlice.actions;
+
+export default customerSlice.reducer;
