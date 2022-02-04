72fc03b04d56e4f0ac133ae2f381a5035665f6f7
hyoni0817 <hyoni0817@naver.com>
2022-02-04 20:42:42 +0900

    Move: CustomerCard컴포넌트 생성 후 코드 분리

--- restaurant/src/App.tsx
+++ restaurant/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import "./App.css";
 import { RootState } from "./app/store";
+import CustomerCard from "./components/CustomerCard";
 import ReservationCard from "./components/ReservationCard";
 import { addReservation } from "./features/reservationSlice";
 
@@ -39,17 +40,8 @@ function App() {
             />
             <button onClick={handleAddReservations}>Add</button>
           </div>
-        </div>
-        <div className="customer-food-container">
-          <div className="customer-food-card-container">
-            <p>Selena Gomez</p>
-            <div className="customer-foods-container">
-              <div className="customer-food"></div>
-              <div className="customer-food-input-container">
-                <input />
-                <button>Add</button>
-              </div>
-            </div>
+          <div className="customer-food-container">
+            <CustomerCard />
           </div>
         </div>
       </div>
