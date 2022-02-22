93e0f21b0c1599d5a187cc4b9270b20859a61828
hyoni0817 <hyoni0817@naver.com>
2022-02-22 23:16:57 +0900

    Feat: uuid 추가

--- restaurant/src/components/ReservationCard.tsx
+++ restaurant/src/components/ReservationCard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { removeReservation } from "../features/reservationSlice";
 import { addCustomer } from "../features/customerSlice";
+import { v4 as uuid } from "uuid";
 
 interface ReservationCardTypes {
   name: string;
